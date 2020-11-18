33969b43b5ff7f428d362b1d9ce5cc8f19bc7e49
vLCJhE9DAzvAQpM <przemyslaw.glabicki@gmail.com>
2020-11-18 14:48:43 +0100

    Setup OrderController with default data. Create basic OrderDto for basic OrderController data.

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,27 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.OrderDto;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class OrderController {
+
+    public List<OrderDto> getOrders() {
+        return new ArrayList<>();
+    }
+
+    public void createOrder (OrderDto orderDto) {}
+
+    public OrderDto getOrder(Long orderId) {
+        return new OrderDto(1L, "Order number 1", "List of items");
+    }
+
+    public OrderDto updateOrder(OrderDto orderDto) {
+        return new OrderDto(1l, "Updated Order number 1", "Updated list of items");
+    }
+
+    public void deleteOrder(Long orderId) {}
 }
