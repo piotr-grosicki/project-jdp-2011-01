5beec52e535f8f3f5471f845df343cab5769ab41
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-11-19 14:54:38 +0100

    Task JDP201101-2

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    private GroupDto groupDto;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getAllGroups")
+    public List<GroupDto> getAllGroups(){
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup")
+    public void createGroup(@RequestBody GroupDto groupDto){
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroup")
+    public GroupDto getGroup(@RequestParam Long groupId) {
+        return new GroupDto(1L, "Test Get");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto(2L, "Test Updated");
+    }
 }
