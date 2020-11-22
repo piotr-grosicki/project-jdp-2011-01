b30280e2355e17a7b9d5b53eef53aaffd767cf63
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-11-22 22:35:35 +0100

    JDP201101-32

--- src/main/java/com/kodilla/ecommercee/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/UserDto.java
@@ -1,17 +1,16 @@
 package com.kodilla.ecommercee;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String userName;
     private String userSurname;
     private Boolean isBlocked;
     private String userKey;
-
-    public UserDto(Long id, String userName, String userSurname, Boolean isBlocked, String userKey) {
-        this.id = id;
-        this.userName = userName;
-        this.userSurname = userSurname;
-        this.isBlocked = isBlocked;
-        this.userKey = userKey;
-    }
 }
