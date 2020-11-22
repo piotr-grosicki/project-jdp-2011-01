a58a4d603fd57d07c7d38f5567d22121c527a7d2
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-11-22 21:55:06 +0100

    JDP201101-29 fixed bugs that were found during tests performed in Postman-JDP201101-2

--- src/main/java/com/kodilla/ecommercee/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/GroupDto.java
@@ -1,22 +1,22 @@
 package com.kodilla.ecommercee;
 
-public class GroupDto {
+public class GroupDto{
     private Long id;
     private String groupName;
 
-    public GroupDto(Long id, String groupName) {
+    public GroupDto(Long id, String groupName){
         this.id = id;
         this.groupName = groupName;
     }
 
-    public GroupDto() {
+    public GroupDto(){
     }
 
-    public Long getId() {
+    public Long getId(){
         return id;
     }
 
-    public String getGroupName() {
+    public String getGroupName(){
         return groupName;
     }
 }
