623dcd4ada041984cdb66b89cb3734a41e98a440
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-11-24 21:12:30 +0100

    JDP201101-3 - takie aby spuulować maina

--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -1,27 +1,16 @@
 package com.kodilla.ecommercee.domain;
 
-public class CartDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDto {
     private long cartId;
     private long userId;
     private String listOfProducts;
-    public CartDto(final long cartId, final long userId, String listOfProducts) {
-        this.cartId = cartId;
-        this.userId = userId;
-        this.listOfProducts = listOfProducts;
-    }
-    public long getCartId() {
-        return cartId;
-    }
-    public long getUserId() {
-        return userId;
-    }
-    public String getListOfProducts() {
-        return listOfProducts;
-    }
-    public void setListOfProducts(String listOfProducts) {
-        this.listOfProducts = listOfProducts;
-    }
 }
 
 
