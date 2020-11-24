b616e978cab6ff6f96ae4b5e731b9ea52f9225e5
vLCJhE9DAzvAQpM <przemyslaw.glabicki@gmail.com>
2020-11-24 22:22:17 +0100

    Add entity: Order.java (with OrderDao + empty entities required by Order)

--- src/main/java/com/kodilla/ecommercee/repository/OrderDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderDao.java
@@ -2,7 +2,12 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface OrderDao extends CrudRepository<Order, Integer> {
 
 }
--- src/main/java/com/kodilla/ecommercee/repository/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserDao.java
@@ -2,6 +2,11 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface UserDao extends CrudRepository<User, Integer> {
 }
