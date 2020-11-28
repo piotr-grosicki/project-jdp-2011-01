f741bbe14fb9a233c69a5203f01957738fa7e6e7
Adam Rozycki <adamrozycki00@gmail.com>
2020-11-28 14:39:04 +0100

    Implement UserMapper methods

--- src/main/java/com/kodilla/ecommercee/user/UserMapper.java
+++ src/main/java/com/kodilla/ecommercee/user/UserMapper.java
@@ -1,4 +1,25 @@
 package com.kodilla.ecommercee.user;
 
 public class UserMapper {
+
+    public UserEntity mapToUserEntity(final UserDto userDto) {
+        return new UserEntity(
+                userDto.getId(),
+                userDto.getUserName(),
+                userDto.getUserSurname(),
+                userDto.getIsBlocked(),
+                userDto.getUserKey()
+        );
+    }
+
+    public UserDto mapToUserDto(final UserEntity user) {
+        return new UserDto(
+                user.getId(),
+                user.getUserName(),
+                user.getUserSurname(),
+                user.getIsBlocked(),
+                user.getUserKey()
+        );
+    }
+
 }
