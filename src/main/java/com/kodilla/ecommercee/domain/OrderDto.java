4a27064c535ce79343b9639195bc769677aac5cc
Adam Rozycki <adamrozycki00@gmail.com>
2020-12-02 17:05:38 +0100

    Add field `products` into OrderDto

--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -5,14 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
+
     private Long orderId;
     private Date orderDate;
+    private List<ProductEntity> products;
     private UserEntity userEntity;
+
 }
 
 
