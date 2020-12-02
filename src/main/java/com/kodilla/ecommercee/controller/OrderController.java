ed1b4cda1b6679a0858b2e60f4b3cc84d874f94d
Adam Rozycki <adamrozycki00@gmail.com>
2020-12-02 18:01:43 +0100

    Implement methods in OrderController

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,33 +1,55 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.OrderDto;
-import org.springframework.web.bind.annotation.*;
+import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.OrderService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/v1/order")
 public class OrderController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
-    public List<OrderDto> getOrders() {
-        return new ArrayList<>();
+    private OrderService service;
+    private OrderMapper mapper;
+
+    @RequestMapping(method = GET, value = "/getAll")
+    public List<OrderDto> getAllOrders() {
+        return mapper.mapToOrderDtoList(service.getAllOrders());
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
-    public void createOrder (@RequestBody OrderDto orderDto) {}
-
-    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
-    public OrderDto getOrder(@RequestParam Long orderId) {
-        return new OrderDto(1L, "Order number 1", "List of items");
+    @RequestMapping(method = POST, value = "/create", consumes = APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody OrderDto orderDto) {
+        service.saveOrder(mapper.mapToOrderEntity(orderDto));
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    @RequestMapping(method = GET, value = "/get")
+    public OrderDto getOrder(@RequestParam final Long orderId) {
+        return mapper.mapToOrderDto(service.getOrder(orderId)
+                .orElseThrow(() -> new NoSuchElementException("No order with id " + orderId))
+        );
+    }
+
+    @RequestMapping(method = PUT, value = "/update")
     public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
-        return new OrderDto(1l, "Updated Order number 1", "Updated list of items");
+        return mapper.mapToOrderDto(service.saveOrder(mapper.mapToOrderEntity(orderDto)));
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
-    public void deleteOrder(@RequestParam Long orderId) {}
+    @RequestMapping(method = DELETE, value = "/delete")
+    public void deleteOrder(@RequestParam Long orderId) {
+        try {
+            service.deleteOrder(orderId);
+        } catch (Exception e) {
+            throw new NoSuchElementException("No order with id " + orderId);
+        }
+    }
+
 }
--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -3,6 +3,9 @@ package com.kodilla.ecommercee.mapper;
 import com.kodilla.ecommercee.domain.OrderDto;
 import com.kodilla.ecommercee.domain.OrderEntity;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OrderMapper {
 
     public OrderEntity mapToOrderEntity(final OrderDto orderDto) {
@@ -19,6 +22,17 @@ public class OrderMapper {
                 order.getProducts(),
                 order.getUserEntity()
         );
+    }
+
+    public List<OrderDto> mapToOrderDtoList(final List<OrderEntity> orders) {
+        return orders.stream()
+                .map(order -> new OrderDto(
+                                order.getOrderDate(),
+                                order.getProducts(),
+                                order.getUserEntity()
+                        )
+                )
+                .collect(Collectors.toList());
     }
 
 }
