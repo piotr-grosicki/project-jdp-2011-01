8bb06b1eeab30aaf627df96e2ddbbfd1f2754cc9
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-12-09 21:54:40 +0100

    task-JDP201101-22 modification of the repository after adding service and mapper for GroupController

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
 public interface GroupRepository extends CrudRepository<GroupEntity, Long> {
-
+    @Override
+    List<GroupEntity> findAll();
 }
