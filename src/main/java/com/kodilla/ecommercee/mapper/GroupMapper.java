dd778b519db0e3a7d586c45a018dd4f0686e7b82
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-12-09 21:51:19 +0100

    task-JDP201101-22 cration mapper for GroupController

--- src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -1,4 +1,30 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.GroupDto;
+import com.kodilla.ecommercee.domain.GroupEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class GroupMapper {
+    public GroupEntity mapToGroup(final GroupDto groupDto) {
+        return new GroupEntity(
+                groupDto.getGroupName()
+        );
+    }
+
+    public GroupDto mapToGroupDto(final GroupEntity groupEntity) {
+        return new GroupDto(
+                groupEntity.getId(),
+                groupEntity.getName()
+        );
+    }
+
+    public List<GroupDto> maptoProductGroupDtoList(final List<GroupEntity> groupEntityList) {
+        return groupEntityList.stream()
+                .map(this::mapToGroupDto)
+                .collect(Collectors.toList());
+    }
 }
