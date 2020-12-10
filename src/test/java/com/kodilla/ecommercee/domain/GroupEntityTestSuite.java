455c8bfeefd6408146566a77a255159220e5ff73
unknown <amienkowski@gmail.com>
2020-12-10 18:58:33 +0100

    First commit

---
+++ src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
@@ -0,0 +1,15 @@
+package com.kodilla.ecommercee.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupEntityTestSuite {
+    @Test
+    public void setUp() {
+
+    }
+}
