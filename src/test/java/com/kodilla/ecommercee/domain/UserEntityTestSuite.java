08314c22224d8b7becc4bef44b5b7cede006195e
Adam Rozycki <adamrozycki00@gmail.com>
2020-12-10 13:27:17 +0100

    Refactor UserEntityTestSuite.shouldSaveNewUsers

--- src/test/java/com/kodilla/ecommercee/domain/UserEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/UserEntityTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.domain;
 
-import com.google.common.collect.Iterables;
 import com.kodilla.ecommercee.repository.UserRepository;
 import com.kodilla.ecommercee.service.UserService;
 import org.junit.After;
@@ -46,7 +45,7 @@ public class UserEntityTestSuite {
         try {
             List<UserEntity> savedUsers = repository.findAll();
             for (UserEntity user : savedUsers) {
-                repository.deleteById(user.getUserId());
+                repository.deleteById(user.getId());
             }
         } catch (Exception e) {
             throw new IllegalStateException("The clean-up method in UserEntityTestSuite not executed properly.");
@@ -55,15 +54,22 @@ public class UserEntityTestSuite {
 
     @Test
     public void shouldSaveNewUsers() {
-        //given & when & then
-        assertEquals(2, Iterables.size(repository.findAll()));
+        //given
+        cleanUp();
+
+        //when
+        user1 = new UserEntity("John Smith", "password", "jsmith@gmail.com");
+        user2 = new UserEntity("Jackie Brown", "password", "jbrown@gmail.com");
+
+        //then
+        assertEquals(2, repository.findAll().size());
     }
 
     @Test
     public void shouldRetrieveSavedUsers() {
         //given & when
-        UserEntity retrievedUser1 = service.getUser(user1.getUserId()).orElse(null);
-        UserEntity retrievedUser2 = service.getUser(user2.getUserId()).orElse(null);
+        UserEntity retrievedUser1 = service.getUser(user1.getId()).orElse(null);
+        UserEntity retrievedUser2 = service.getUser(user2.getId()).orElse(null);
 
         //then
         assertEquals(user1, retrievedUser1);
@@ -73,8 +79,8 @@ public class UserEntityTestSuite {
     @Test
     public void shouldRetrieveBlockedUser() {
         //given & when
-        service.blockUser(user1.getUserId());
-        UserEntity retrievedUser = service.getUser(user1.getUserId()).orElse(user2);
+        service.blockUser(user1.getId());
+        UserEntity retrievedUser = service.getUser(user1.getId()).orElse(user2);
 
         //then
         assertEquals(false, user2.getIsBlocked());
@@ -84,7 +90,7 @@ public class UserEntityTestSuite {
     @Test
     public void shouldReturnBlockedUser() {
         //given & when
-        UserEntity returnedUser = service.blockUser(user1.getUserId()).orElse(user2);
+        UserEntity returnedUser = service.blockUser(user1.getId()).orElse(user2);
 
         //then
         assertEquals(false, user2.getIsBlocked());
@@ -94,15 +100,15 @@ public class UserEntityTestSuite {
     @Test
     public void shouldDeleteUser() {
         //given
-        long countSavedUsersBeforeDeletion = Iterables.size(repository.findAll());
-        long deletedUserId = user1.getUserId();
+        long countSavedUsersBeforeDeletion = repository.findAll().size();
+        long deletedUserId = user1.getId();
 
         //when
         repository.deleteById(deletedUserId);
         UserEntity deletedUser = service.getUser(deletedUserId).orElse(null);
 
         //then
-        assertEquals(countSavedUsersBeforeDeletion - 1, Iterables.size(repository.findAll()));
+        assertEquals(countSavedUsersBeforeDeletion - 1, repository.findAll().size());
         assertNull(deletedUser);
     }
 
