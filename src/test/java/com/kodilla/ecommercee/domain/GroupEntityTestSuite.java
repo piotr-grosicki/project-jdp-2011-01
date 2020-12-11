9b4984dfe4782367e70a21792311a4f658713f0c
unknown <amienkowski@gmail.com>
2020-12-11 19:37:25 +0100

    Work in progress

--- src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
@@ -1,21 +1,30 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.repository.GroupRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GroupEntityTestSuite {
+    @Autowired
+    GroupRepository groupRepo;
+
     @Test
-    public void shouldAddSingleGroup() {
+    public void shouldCreateAndReadSingleGroup() {
         //Given
-
+        groupRepo.deleteAll();
+        GroupEntity testGroup = new GroupEntity("test name");
         //When
-
+        groupRepo.save(testGroup);
+        System.out.println(testGroup.getId());
         //Then
-
+        Assert.assertEquals(1, groupRepo.count());
+//        Assert.assertEquals("test name", groupRepo.findById(1L));
     }
 
     @Test
