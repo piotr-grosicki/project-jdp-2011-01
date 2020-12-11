5c5eed1126e67ccfaaab5f223837b1771c882781
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-12-11 23:03:18 +0100

    added List of products to Dto

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class GroupDto {
     private Long id;
     private String groupName;
+    private List<ProductDto> productDtoList;
 }
