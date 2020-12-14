4aec6bbc6c2b4fc7f7ecd04279abc2e737857aa7
unknown <amienkowski@gmail.com>
2020-12-14 21:33:36 +0100

    Still in progress.

--- src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
@@ -1,9 +1,7 @@
 package com.kodilla.ecommercee.domain;
 
-import com.kodilla.ecommercee.repository.CartRepository;
 import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
-import com.kodilla.ecommercee.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,32 +21,65 @@ public class GroupEntityTestSuite {
     ProductRepository productRepo;
 
     @Test
-    public void shouldCreateAndReadSingleGroup() {
+    public void groupEntityGeneratesId() {
         //Given
         groupRepo.deleteAll();
-        GroupEntity testGroup = new GroupEntity("test name");
+        GroupEntity testGroup1 = new GroupEntity("test name");
+        GroupEntity testGroup2 = new GroupEntity("test name");
         //When
-        groupRepo.save(testGroup);
-        Optional<GroupEntity> receivedGroup = groupRepo.findById(testGroup.getId());
+        groupRepo.save(testGroup1);
+        groupRepo.save(testGroup2);
         //Then
-        assertTrue(groupRepo.existsById(testGroup.getId()));
-        assertTrue(receivedGroup.isPresent());
-        receivedGroup.ifPresent(group -> assertEquals("test name", group.getName()));
+        assertTrue(testGroup1.getId() != null && testGroup2.getId() != null);
+        assertNotEquals(testGroup1.getId(), testGroup2.getId());
     }
 
 //    @Test
-//    public void shouldUpdateExistingGroup() {
+//    public void groupEntitySettersAndGettersTest() {
 //        //Given
 //        groupRepo.deleteAll();
 //        GroupEntity testGroup = new GroupEntity("test name");
 //        groupRepo.save(testGroup);
+//        Long id = testGroup.getId();
 //        //When
-//        groupRepo.findById(testGroup.getId()).ifPresent(group -> group.setName("another name"));
+//        groupRepo.findById(id).ifPresent(group -> System.out.println("TEST: "+group.getTest().toString()));
+//        groupRepo.findById(id).ifPresent(group -> System.out.println("TEST: "+(group.getTest()!=null)));
 //        //Then
-//        Optional<GroupEntity> receivedGroup = groupRepo.findById(testGroup.getId());
-//        assertTrue(receivedGroup.isPresent());
-//        receivedGroup.ifPresent(group -> assertEquals("another name", group.getName()));
+//
 //    }
+
+    @Test
+    public void shouldCreateAndReadSingleGroup() {
+        //Given
+        groupRepo.deleteAll();
+        GroupEntity testGroup = new GroupEntity("test name");
+        //When&Then
+        groupRepo.save(testGroup);
+        assertTrue(groupRepo.existsById(testGroup.getId()));
+        Optional<GroupEntity> receivedGroup = groupRepo.findById(testGroup.getId());
+        assertTrue(receivedGroup.isPresent());
+        receivedGroup.ifPresent(group -> assertEquals("test name", group.getName()));
+    }
+
+    @Test
+    public void shouldUpdateExistingGroup() {
+        //Given
+        GroupEntity testGroup = new GroupEntity("test name");
+        ProductEntity testProduct1 = new ProductEntity("test name", "description", 1.0, null);
+        ProductEntity testProduct2 = new ProductEntity("test name", "description", 1.0, null);
+        groupRepo.save(testGroup);
+        Long id = testGroup.getId();
+        String entryName = "test name";
+        //When
+        testGroup.setName("another name");
+        testProduct1.setGroup(testGroup);
+        testProduct2.setGroup(testGroup);
+        testGroup.getProducts();
+        //Then
+        Optional<GroupEntity> receivedGroup = groupRepo.findById(id);
+        assertTrue(receivedGroup.isPresent());
+        receivedGroup.ifPresent(group -> assertEquals("another name", group.getName()));
+    }
 
     @Test
     public void shouldRemoveSingleGroup() {
@@ -71,12 +102,12 @@ public class GroupEntityTestSuite {
         groupRepo.save(testGroup);
         ProductEntity testProduct1 = new ProductEntity("test name", "description", 1.0, null);
         ProductEntity testProduct2 = new ProductEntity("test name", "description", 1.0, null);
-//        testProduct1.setGroup(testGroup);
-//        testProduct2.setGroup(testGroup);
+        testProduct1.setGroup(testGroup);
+        testProduct2.setGroup(testGroup);
         productRepo.save(testProduct1);
         productRepo.save(testProduct2);
         //When
-        groupRepo.deleteById(testGroup.getId());
+//        groupRepo.deleteById(testGroup.getId());
         //Then
         assertTrue(productRepo.existsById(testProduct1.getId()));
         assertTrue(productRepo.existsById(testProduct2.getId()));
