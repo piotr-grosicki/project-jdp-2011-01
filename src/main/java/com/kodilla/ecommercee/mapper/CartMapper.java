eb35ab632596fcfd7c610681e0c5281c062a0e22
lukaszlizewski <lizewski.lukasz@gmail.com>
2020-12-14 20:19:57 +0100

    JDP201101-23_1 imports clean

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -1,7 +1,5 @@
 package com.kodilla.ecommercee.mapper;
 
-import com.kodilla.ecommercee.domain.CartEntity;
-import com.kodilla.ecommercee.domain.OrderEntity;
 import com.kodilla.ecommercee.domain.ProductDto;
 import com.kodilla.ecommercee.domain.ProductEntity;
 import org.springframework.stereotype.Component;
