79df49701849e8ff22759fbc3a5826e734213d58
unknown <amienkowski@gmail.com>
2020-12-15 11:05:10 +0100

    Third test - EAGER try.

--- src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupEntityTestSuite.java
@@ -57,29 +57,29 @@ public class GroupEntityTestSuite {
         receivedGroup.ifPresent(group -> assertEquals("test name", group.getName()));
     }
 
-//    @Test
-//    public void shouldUpdateExistingGroupTest() {
-//        //Given
-//        groupRepo.deleteAll();
-//        productRepo.deleteAll();
-//        GroupEntity testGroup = new GroupEntity("test name");
-//        ProductEntity testProduct1 = new ProductEntity("test name", "description", 1.0, null);
-//        ProductEntity testProduct2 = new ProductEntity("test name", "description", 1.0, null);
-//        groupRepo.save(testGroup);
-//        Long id = testGroup.getId();
-//        String secondName = "new name";
-//        //When
-//        testProduct1.setGroup(testGroup);
-//        testProduct2.setGroup(testGroup);
-//        productRepo.save(testProduct1);
-//        productRepo.save(testProduct2);
-//        testGroup.setName(secondName);
-//        groupRepo.save(testGroup);
-//        Optional<GroupEntity> retrievedGroup = groupRepo.findById(id);
-//        //Then
-//        retrievedGroup.ifPresent(g -> assertEquals(2, g.getProducts().size()));
-//        retrievedGroup.ifPresent(g -> assertEquals(secondName, g.getName()));
-//    }
+    @Test
+    public void shouldUpdateExistingGroupTest() {
+        //Given
+        groupRepo.deleteAll();
+        productRepo.deleteAll();
+        GroupEntity testGroup = new GroupEntity("test name");
+        ProductEntity testProduct1 = new ProductEntity("test name", "description", 1.0, null);
+        ProductEntity testProduct2 = new ProductEntity("test name", "description", 1.0, null);
+        groupRepo.save(testGroup);
+        Long id = testGroup.getId();
+        String secondName = "new name";
+        //When
+        testProduct1.setGroup(testGroup);
+        testProduct2.setGroup(testGroup);
+        productRepo.save(testProduct1);
+        productRepo.save(testProduct2);
+        testGroup.setName(secondName);
+        groupRepo.save(testGroup);
+        Optional<GroupEntity> retrievedGroup = groupRepo.findById(id);
+        //Then
+        retrievedGroup.ifPresent(g -> assertEquals(2, g.getProducts().size()));
+        retrievedGroup.ifPresent(g -> assertEquals(secondName, g.getName()));
+    }
 
     @Test
     public void shouldRemoveSingleGroup() {
